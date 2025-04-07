489667558358367d4a3551eab3c9ba91f7a981eb
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-04-07 13:03:37 -0500

    Will add tests and code to create working create_factories method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,4 +1,14 @@
 class FacilityFactory
   def initialize
   end
+
+  def create_factories(facility_data)
+    facility_data.map do |data|
+      facility = Facility.new({
+        name: data[:dmv_office],
+        address: data[:address_li],
+        phone: data[:phone]
+      })
+    end
+  end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -4,9 +4,20 @@ RSpec.describe FacilityFactory do
   before(:each) do
     @facility_factory = FacilityFactory.new
     @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+    # binding.pry
   end
 
   it 'exists' do
     expect(@facility_factory).to be_a(FacilityFactory)
+  end
+
+  it 'creates a CO location' do
+    facilities = @facility_factory.create_factories(@co_dmv_office_locations)
+    expect(facilities).to be_a(Array)
+    
+    expect(facilities[0]).to be_a(Facility)
+    expect(facilities[0].name).to eq("DMV Tremont Branch")
+    expect(facilities[0].address).to eq("2855 Tremont Place")
+    expect(facilities[0].phone).to eq("(720) 865-4600")
   end
 end 
