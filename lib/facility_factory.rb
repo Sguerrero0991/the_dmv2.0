77a0e20935b9d0d5d093115407de432640d2d64f
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-04-07 14:57:08 -0500

    Wrote tests and code to write working create_factories method

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -2,13 +2,27 @@ class FacilityFactory
   def initialize
   end
 
-  def create_factories(facility_data)
+  def create_factories(location, facility_data)
     facility_data.map do |data|
+      if location == "Colorado"
       facility = Facility.new({
         name: data[:dmv_office],
         address: data[:address_li],
         phone: data[:phone]
       })
-    end
+      elsif location == "New York"
+        facility = Facility.new({
+        name: data[:office_name],
+        address: data[:street_address_line_1],
+        phone: data[:public_phone_number]
+      })
+      elsif location == "Missouri"
+      end 
+    end 
   end
 end
+
+# extra agruement, if the location is NY 
+# if it's NY location 
+#   we're only adding additonal services
+# end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe FacilityFactory do
   before(:each) do
     @facility_factory = FacilityFactory.new
     @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+    @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
+    
     # binding.pry
   end
 
@@ -12,12 +14,32 @@ RSpec.describe FacilityFactory do
   end
 
   it 'creates a CO location' do
-    facilities = @facility_factory.create_factories(@co_dmv_office_locations)
+    facilities = @facility_factory.create_factories("Colorado", @co_dmv_office_locations)
     expect(facilities).to be_a(Array)
     
     expect(facilities[0]).to be_a(Facility)
     expect(facilities[0].name).to eq("DMV Tremont Branch")
     expect(facilities[0].address).to eq("2855 Tremont Place")
     expect(facilities[0].phone).to eq("(720) 865-4600")
+  end
+
+  it 'creates a NY location' do
+    facilities = @facility_factory.create_factories("New York", @new_york_facilities = DmvDataService.new.ny_dmv_office_locations)
+    expect(facilities).to be_a(Array)
+    
+    expect(facilities[0]).to be_a(Facility)
+    expect(facilities[0].name).to eq("LAKE PLACID")
+    expect(facilities[0].address).to eq("2693 MAIN STREET")
+    expect(facilities[0].phone).to eq(nil)
+  end
+
+  it 'creates a CO location' do
+    facilities = @facility_factory.create_factories("Missouri", )
+    expect(facilities).to be_a(Array)
+    
+    expect(facilities[0]).to be_a(Facility)
+    expect(facilities[0].name).to eq("LAKE PLACID")
+    expect(facilities[0].address).to eq("2693 MAIN STREET")
+    expect(facilities[0].phone).to eq(nil)
   end
 end 
